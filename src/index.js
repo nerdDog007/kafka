31013089b627ce774d004e20adb4bb5fa883a93f
nerdDog007 <155296029+nerdDog007@users.noreply.github.com>
2024-09-15 11:10:26 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -120,14 +120,13 @@ import './style.css'
             <div className="desc full">
               <h4 className="subtitle">My name is Sandesh.</h4>
               <p>
-                I am a web developer and UX designer based in the beautiful west coast of Spain,
-                Galicia.
+                I am a web developer and UX designer based in the beautiful City Kathmandu In Nepal.
               </p>
               <p>
                 I really enjoy solving problems as well as making things pretty and easy to use. I
-                can't stop learning new things; the more, the better. I also love photography, a hobby
-                I'm taking along since the good old film cameras. Oh, and rice with milk; I have a
-                passion for rice with milk!
+                can't stop learning new things; the more, the better. I also love writing, a hobby
+                I'm taking along since the long time. Oh, and watching movie with popcorn; I have a
+                passion for movie with popcorn!
               </p>
             </div>
             <div className="title">
@@ -138,21 +137,18 @@ import './style.css'
               <h4 className="subtitle">I'm a programmer.</h4>
               <p>
                 For the front-end I usually work with Javascript, either standalone or including
-                popular frameworks like ReactJS and VueJS. I also make the web pretty by using Sass,
-                CSS and, whenever needed, any of their friends: Bootstrap, Bulma, etc.
+                popular frameworks like ReactJS. I also make the web pretty by using Sass,
+                CSS and, whenever needed, any of their friends: tailwindCss.
               </p>
               <p>
-                For the back-end I also work with Javascript (NodeJS, Express, MongoDB, etc). But, of
-                course, whenever the project requires PHP, I do PHP as well (Wordpress, Laravel, etc).
+                For the back-end I also work with Javascript (NodeJS, Express, MongoDB, etc).
               </p>
             </div>
             <div className="desc">
               <h4 className="subtitle">Also a designer.</h4>
               <p>
                 I feel comfortable working with many Adobe products. Photoshop, Illustrator, InDesign,
-                Lightroom or Xd are some kind of industry standards and I love working with them. I'm
-                not limited to them, though: Gimp, Inkscape or Figma are also very valid applications
-                that I've been working with.
+                Lightroom or Xd are some kind of industry standards and I love working with them.
               </p>
               <p>
                 User interfaces, brochures, books, branding... You name it! As I mentioned, creating
